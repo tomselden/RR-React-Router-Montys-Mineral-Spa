6ae16d6bcf7902443ffa54da26c0e815d4857296
Thomas Selden <thomasselden42@gmail.com>
2022-02-07 13:19:10 -0500

    fixed starting script in package.json

--- react-router/src/components/Home.js
+++ react-router/src/components/Home.js
@@ -1,15 +1,13 @@
 
 
-export default function Home () {
-    return(
-        <div className="homepage">
-            <h1>Visit our resort and change your life!</h1>
+export default function Home() {
+  return (
+    <div className="homepage">
+      <h1>Visit our resort and change your life!</h1>
 
-            <h1>Grand opening in StressVille, Texas</h1>
-            
-            <h1>Check out our wide array of packages and offers!</h1>
+      <h1>Grand opening in StressVille, Texas</h1>
 
-        </div>
-    )
-
+      <h1>Check out our wide array of packages and offers!</h1>
+    </div>
+  );
 }
